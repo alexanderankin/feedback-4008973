c35389eae1e6d448b41487f29bf84599facfbf94
David Ankin <daveankin@gmail.com>
2022-05-01 13:19:50 -0400

    attempt with spring application

--- src/main/java/springapp/SpringApp.java
+++ src/main/java/springapp/SpringApp.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Mono;
-
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 @SpringBootApplication
 public class SpringApp {
     public static void main(String[] args) {
@@ -16,9 +18,26 @@ public class SpringApp {
 
     @Controller
     public static class ExampleController {
+        private static final Scheduler schedulerFromLib = Schedulers.boundedElastic();
+        private static final Scheduler schedulerCustom = Schedulers.newBoundedElastic(5, 10, "schedulerCustom");
+
+        @SuppressWarnings("DuplicatedCode") // for debugging the ide!
         @ResponseBody
         @RequestMapping
-        Mono<String> get() {
+        Mono<String> get(@RequestParam(required = false) String block) {
+            // issues warning, the expected way
+            //noinspection CallingSubscribeInNonBlockingScope
+            Mono.fromCallable(() -> 1).subscribe();
+
+            // no warning issued, also expected
+            Mono.fromCallable(() -> 1).subscribeOn(schedulerFromLib).subscribe();
+
+            // no warning issued, unlike the issue I thought I was seeing
+            Mono.fromCallable(() -> 1).subscribeOn(schedulerCustom).subscribe();
+
+            // the only real issue, no warning issued
+            Mono.fromCallable(() -> { Thread.sleep(100); return 1; }).subscribeOn(Schedulers.immediate()).subscribe();
+
             return Mono.just("hi");
         }
     }
--- src/test/java/springapp/SpringAppTest.java
+++ src/test/java/springapp/SpringAppTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Objects;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringAppTest {
     @Autowired
@@ -14,7 +16,7 @@ public class SpringAppTest {
     @Test
     void test() {
         EntityExchangeResult<String> result = webTestClient.get().uri("/").exchange().expectBody(String.class).returnResult();
-        if (!result.getResponseBody().equals("hi")) {
+        if (!Objects.equals(result.getResponseBody(), "hi")) {
             throw new AssertionError("expected 'hi', got: " + result.getResponseBody());
         }
     }

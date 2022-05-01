3f4bbc0364f338b0314ecfd80158019dc19863ee
David Ankin <daveankin@gmail.com>
2022-05-01 13:21:31 -0400

    clean up and spacing

--- src/main/java/springapp/SpringApp.java
+++ src/main/java/springapp/SpringApp.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
+
 @SpringBootApplication
 public class SpringApp {
     public static void main(String[] args) {
@@ -24,7 +24,7 @@ public class SpringApp {
         @SuppressWarnings("DuplicatedCode") // for debugging the ide!
         @ResponseBody
         @RequestMapping
-        Mono<String> get(@RequestParam(required = false) String block) {
+        Mono<String> get() {
             // issues warning, the expected way
             //noinspection CallingSubscribeInNonBlockingScope
             Mono.fromCallable(() -> 1).subscribe();
